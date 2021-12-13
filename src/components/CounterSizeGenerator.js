12b52e85bb4667b7fdffb85e21f6764949338eb3
Marcus Ho <marcus.ho@oocl.com>
2021-12-13 17:31:13 +0800

    [ReactDay1][marcus] feat: error handling

--- src/components/CounterSizeGenerator.js
+++ src/components/CounterSizeGenerator.js
@@ -4,9 +4,26 @@ function CounterSizeGenerator(props) {
   const [size, setSize] = useState(0);
 
   function handleChangeSize(event) {
-    const size = parseInt(event.target.value);
-    setSize(size);
-    props.updateSize(size);
+    if (isNaN(parseInt(event.target.value))) {
+      setSize(0);
+      props.updateSize(0);
+      return;
+    }
+
+    if (parseInt(event.target.value) < 0) {
+      setSize(0);
+      props.updateSize(0);
+      return;
+    }
+
+    if (parseInt(event.target.value) > 99) {
+      setSize(99);
+      props.updateSize(99);
+      return;
+    }
+
+    setSize(parseInt(event.target.value));
+    props.updateSize(parseInt(event.target.value));
   }
 
   return (
